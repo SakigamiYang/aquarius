07fcaa20c0a11fddc0217e4b8813d6b833f36158
SakigamiYang <sakigami@163.com>
2020-07-08 00:42:09 +0800

    add test for SparkJob

---
+++ aquarius-common/src/test/scala/me/sakigami_yang/aquarius/common/spark/SparkJobSpec.scala
@@ -0,0 +1,44 @@
+package me.sakigami_yang.aquarius.common.spark
+
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+import org.apache.spark.sql.functions._
+
+class SparkJobSpec extends AnyFunSpec with Matchers {
+  describe("test configurations for SparkJob") {
+    it("config is default") {
+      class AJob extends SparkJob {
+        override def run(): Unit = {
+          // do nothing
+        }
+      }
+
+      val ajob = new AJob
+      ajob.jobName shouldBe "local-spark-job"
+      ajob.master shouldBe "local[*]"
+      ajob.isLocal shouldBe true
+      ajob.run()
+      ajob.stop()
+    }
+
+    it("config is customized") {
+      class BJob extends SparkJob {
+        override val jobName: String = "b-job"
+        override val master: String = "local[1]"
+
+        override def run(): Unit = {
+          import spark.implicits._
+          val df = spark.createDataFrame(Seq((1, "1"), (2, "2"), (3, "c"))).toDF("col1", "col2")
+          df.select("col1").agg(sum("col1")).collect()(0).getLong(0) shouldBe 6L
+        }
+      }
+
+      val bjob = new BJob
+      bjob.jobName shouldBe "b-job"
+      bjob.master shouldBe "local[1]"
+      bjob.isLocal shouldBe true
+      bjob.run()
+      bjob.stop()
+    }
+  }
+}
